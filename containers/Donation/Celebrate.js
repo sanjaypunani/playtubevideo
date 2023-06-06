a32c45f85139f5ea83bd89e9b469e70aa2529ca1
Sanjay Punani <punaniparas1997@gmail.com>
2023-06-06 20:35:06 +0530

    Remove auto suggection

--- containers/Donation/Celebrate.js
+++ containers/Donation/Celebrate.js
@@ -1,13 +1,13 @@
-import React from "react"
-import css from "./celebrate.css"
+import React from 'react';
+// import css from "./celebrate.css"
 
-const Index = (props) => {
-    return (
-        <div className="pyro">
-            <div className="before"></div>
-            <div className="after"></div>
-        </div>
-    )
-}
+const Index = props => {
+  return (
+    <div className="pyro">
+      <div className="before"></div>
+      <div className="after"></div>
+    </div>
+  );
+};
 
 export default Index;
