6286e38c8d490e01cedd7936f03d54c29146494a
Sanjay Punani <punaniparas1997@gmail.com>
2023-06-06 20:14:07 +0530

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-
-
-const withCSS = require('@zeit/next-css')
+const withCSS = require('@zeit/next-css');
 const webpack = require('webpack');
-const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
+const withPWA = require('next-pwa');
+const isProd = process.env.NODE_ENV === 'production';
 
 // function HACK_removeMinimizeOptionFromCssLoaders(config) {
 //   console.warn(
@@ -19,28 +17,35 @@ const isProd = process.env.NODE_ENV === 'production'
 //     }
 //   });
 // }
- module.exports = withPWA(withCSS(
-  {
+module.exports = withPWA(
+  withCSS({
     cssLoaderOptions: {
       url: false,
     },
-    assetPrefix: isProd && process.env.CDN_URL_FOR_STATIC_RESOURCES ? process.env.CDN_URL_FOR_STATIC_RESOURCES : '',
+    assetPrefix:
+      isProd && process.env.CDN_URL_FOR_STATIC_RESOURCES
+        ? process.env.CDN_URL_FOR_STATIC_RESOURCES
+        : '',
     publicRuntimeConfig: {
-      localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
-        ? process.env.LOCALE_SUBPATHS
-        : 'none',
+      localeSubpaths:
+        typeof process.env.LOCALE_SUBPATHS === 'string'
+          ? process.env.LOCALE_SUBPATHS
+          : 'none',
     },
     webpack: function (config) {
-      config.plugins.push(new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery'
-      }))
-      return config
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery',
+          'window.jQuery': 'jquery',
+        }),
+      );
+      return config;
     },
     pwa: {
-      disable: process.env.NODE_ENV === 'production' ? false : true,
-      dest: "public"
-    }
-  }
-));
+      // disable: process.env.NODE_ENV === 'production' ? false : true,
+      disable: true,
+      dest: 'public',
+    },
+  }),
+);
