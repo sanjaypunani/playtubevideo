73fa80057961661c87965f9e9689f308cc4cccef
Sanjay Punani <punaniparas1997@gmail.com>
2023-06-06 20:23:54 +0530

    Update config

--- next.config.js
+++ next.config.js
@@ -17,21 +17,41 @@ const isProd = process.env.NODE_ENV === 'production';
 //     }
 //   });
 // }
+
+// module.exports = withPWA(
+//   withCSS({
+//     cssLoaderOptions: {
+//       url: false,
+//     },
+//     assetPrefix:
+//       isProd && process.env.CDN_URL_FOR_STATIC_RESOURCES
+//         ? process.env.CDN_URL_FOR_STATIC_RESOURCES
+//         : '',
+//     publicRuntimeConfig: {
+//       localeSubpaths:
+//         typeof process.env.LOCALE_SUBPATHS === 'string'
+//           ? process.env.LOCALE_SUBPATHS
+//           : 'none',
+//     },
+//     webpack: function (config) {
+//       config.plugins.push(
+//         new webpack.ProvidePlugin({
+//           $: 'jquery',
+//           jQuery: 'jquery',
+//           'window.jQuery': 'jquery',
+//         }),
+//       );
+//       return config;
+//     },
+//     pwa: {
+//       disable: process.env.NODE_ENV === 'production' ? false : true,
+//       dest: 'public',
+//     },
+//   }),
+// );
+
 module.exports = withPWA(
-  withCSS({
-    cssLoaderOptions: {
-      url: false,
-    },
-    assetPrefix:
-      isProd && process.env.CDN_URL_FOR_STATIC_RESOURCES
-        ? process.env.CDN_URL_FOR_STATIC_RESOURCES
-        : '',
-    publicRuntimeConfig: {
-      localeSubpaths:
-        typeof process.env.LOCALE_SUBPATHS === 'string'
-          ? process.env.LOCALE_SUBPATHS
-          : 'none',
-    },
+  {
     webpack: function (config) {
       config.plugins.push(
         new webpack.ProvidePlugin({
@@ -46,5 +66,21 @@ module.exports = withPWA(
       disable: process.env.NODE_ENV === 'production' ? false : true,
       dest: 'public',
     },
-  }),
+  },
+  // withCSS({
+  //   cssLoaderOptions: {
+  //     url: false,
+  //   },
+  //   assetPrefix:
+  //     isProd && process.env.CDN_URL_FOR_STATIC_RESOURCES
+  //       ? process.env.CDN_URL_FOR_STATIC_RESOURCES
+  //       : '',
+  //   publicRuntimeConfig: {
+  //     localeSubpaths:
+  //       typeof process.env.LOCALE_SUBPATHS === 'string'
+  //         ? process.env.LOCALE_SUBPATHS
+  //         : 'none',
+  //   },
+
+  // }),
 );
