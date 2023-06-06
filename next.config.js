42f8fffd749f0f3e552e072ae62892be91e6f68e
Sanjay Punani <punaniparas1997@gmail.com>
2023-06-06 21:33:32 +0530

    Update

--- next.config.js
+++ next.config.js
@@ -17,34 +17,28 @@ const isProd = process.env.NODE_ENV === 'production';
 //     }
 //   });
 // }
-module.exports = withPWA(
-  withCSS({
-    cssLoaderOptions: {
-      url: false,
-    },
-    assetPrefix:
-      isProd && process.env.CDN_URL_FOR_STATIC_RESOURCES
-        ? process.env.CDN_URL_FOR_STATIC_RESOURCES
-        : '',
-    publicRuntimeConfig: {
-      localeSubpaths:
-        typeof process.env.LOCALE_SUBPATHS === 'string'
-          ? process.env.LOCALE_SUBPATHS
-          : 'none',
-    },
-    webpack: function (config) {
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          $: 'jquery',
-          jQuery: 'jquery',
-          'window.jQuery': 'jquery',
-        }),
-      );
-      return config;
-    },
-    pwa: {
-      disable: process.env.NODE_ENV === 'production' ? false : true,
-      dest: 'public',
-    },
-  }),
-);
+module.exports = withCSS({
+  cssLoaderOptions: {
+    url: false,
+  },
+  assetPrefix:
+    isProd && process.env.CDN_URL_FOR_STATIC_RESOURCES
+      ? process.env.CDN_URL_FOR_STATIC_RESOURCES
+      : '',
+  publicRuntimeConfig: {
+    localeSubpaths:
+      typeof process.env.LOCALE_SUBPATHS === 'string'
+        ? process.env.LOCALE_SUBPATHS
+        : 'none',
+  },
+  webpack: function (config) {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+      }),
+    );
+    return config;
+  },
+});
