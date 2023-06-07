4a3116ea87a1a9708a167b743c7c60156031c191
Sanjay Punani <punaniparas1997@gmail.com>
2023-06-07 22:28:36 +0530

    Update

--- containers/Live/WatchLivePopup.js
+++ containers/Live/WatchLivePopup.js
@@ -5,7 +5,12 @@ import ReactPlayer from 'react-player';
 
 export const WatchLivePopup = ({ handleClose, open, data }) => {
   const streamUrl =
-    'http://' + window.location.hostname + ':8000' + data?.stream_url + '.flv';
+    window.location.protocol +
+    '//' +
+    window.location.hostname +
+    ':8000' +
+    data?.stream_url +
+    '.flv';
   return (
     <div className={`modal fade ${open ? 'show' : ''}`} id="watchlivepopup">
       <div className="modal-dialog modal-md modal-dialog-centered modal-xl popupDesign full-screen-dialog">
